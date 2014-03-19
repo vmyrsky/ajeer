f8ab352c5925955bf2fb8b4d8b212713c00e09da
Ethereal666 <ville.myrskyneva@gmail.com>
2014-03-19 16:21:24 +0200

    Trying to configure karma for testing. Partially working, but missing some lib(s) required for testing ('expect' functions not recognized)

---
+++ Angular/karma.conf.js
@@ -0,0 +1,82 @@
+// Karma (test) configuration
+
+module.exports = function(config) {
+  config.set({
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: './src/',
+
+
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['jasmine'],
+
+
+    // list of files / patterns to load in the browser (same as you specify in your 'index.html' [phonebook.html])
+    files: [
+      'main/webapp/app/lib/jquery/jquery-1.10.2.js',
+      'main/webapp/app/lib/jquery/jquery-ui-1.10.3.js',
+      'main/webapp/app/lib/angular/angular.js',
+      'main/webapp/app/lib/angular/angular-translate.min.js',
+      'main/webapp/app/lib/angular/angular-translate-loader-static-files.min.js',
+      'main/webapp/app/lib/angular/angular-translate-loader-url.min.js',
+      'main/webapp/app/js/filters/I18N.js',
+      'main/webapp/app/lib/angular/angular-route.js',
+      'main/webapp/app/js/App.js',
+      'main/webapp/app/js/services/RestServices.js',
+      'main/webapp/app/js/services/ShareDataService.js',
+      'main/webapp/app/js/controllers/MessageControllers.js',
+      'main/webapp/app/js/controllers/MainControllers.js',
+      'main/webapp/app/js/controllers/DetailControllers.js',
+      'main/webapp/app/js/directives/AddDirectives.js',
+      // The test files to execute
+      'test/webapp/app/js/AppTest.js'
+    ],
+
+
+    // list of files to exclude
+    exclude: [
+      
+    ],
+
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {
+    
+    },
+
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['progress'],
+
+
+    // web server port
+    port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['Firefox'],
+
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: false
+  });
+};
