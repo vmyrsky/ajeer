ffb276cc93874f96237a3771783cef1cbb1ce3a1
Ethereal666 <ville.myrskyneva@gmail.com>
2014-03-23 13:02:00 +0200

    This "service" may have been instantiated wrongly, which could explain why the messages are not shown correctly (always) => Will fix this at better time later

--- Angular/src/main/webapp/app/js/services/ShareDataService.js
+++ Angular/src/main/webapp/app/js/services/ShareDataService.js
@@ -1,7 +1,11 @@
 'use strict';
 
-// AngularJS; Best Practice: Prefer using the definition object over returning a
-// function
+//Note: I discovered I may have a bug here because of miss understanding the usage of 'factory', 'service', 'provider'
+//Read: http://stackoverflow.com/questions/15666048/angular-js-service-vs-provider-vs-factory
+//This should be a singleton service instead of a factory service (factories do not provide service as a singleton),
+//I will do the testing needed later and fix this properly
+
+// AngularJS; Best Practice: Prefer using the definition object over returning a function
 var angularPOC = angular.module('angularPOC');
 angularPOC.factory('ShareDataService', [ '$timeout', function($timeout) {
 	var person = '';
